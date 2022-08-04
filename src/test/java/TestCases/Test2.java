b11a91b34262c7005ec0bace855d0dafebdf4f31
Harshal <Harshal@DESKTOP-37EBRU3>
2022-08-04 21:33:34 +0530

    test 1 changed to test 2

--- src/test/java/TestCases/Test2.java
+++ src/test/java/TestCases/Test2.java
@@ -1,12 +1,10 @@
 package TestCases;
 
-public class Test1 {
+public class Test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
-	    System.out.println("Hiii  this edited class in poshmark");
+		System.out.println("hii....");
 
 	}
 
